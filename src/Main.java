17d5fb1646a86d9bd42df636b4f0c08632b00e05
Midget <Midget@DESKTOP-35BCVA8>
2024-09-22 19:12:34 -0500

    yeah

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,188 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.Map;
+
 public class Main {
+    public static boolean pass = true;
+    public static final Set<String> reserved = Set.of("TAGS", "BEGIN", "SEQUENCE", "INTEGER", "DATE", "END");
+    public static final Map<Character, String> charTokens = Map.of(
+            '\"', "QUOTATION MARK",
+            '(', "LEFT PARENTHESIS",
+            ')', "RIGHT PARENTHESIS",
+            ',', "COMMA",
+            '-', "HYPHEN-MINUS",
+            ':', "COLON",
+            '=', "EQUAL SIGN",
+            '{', "LEFT CURLY BRACKET",
+            '|', "VERTICAL LINE",
+            '}', "RIGHT CURLY BRACKET"
+    );
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("did this work?");
+        String file = "src/test.txt";
+        String line;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                evaluateLine(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        if (pass) {
+            System.out.println("\nSuccessfully identified all tokens");
+        } else {
+            System.out.println("\nCould not successfully identify all tokens");
+        }
+
+    }
+
+    public static void evaluateLine (String line) {
+        int i = 0;
+
+        while (i < line.length()) {
+            char c = line.charAt(i);
+
+            if (c == ' ') {
+                i++;
+                continue;
+            }
+
+            if (assignmentOperator(line, i)) {
+                System.out.printf("Assignment Operator: %14s%n", "::=");
+                i += 3;
+            } else if (Character.isLetter(c) && Character.isUpperCase(c)) {
+                String typeReference = getTypeReference(line.substring(i));
+                if (typeReference == null) {
+                    System.out.println("Error: Invalid Type Reference on line \"" + line + "\"");
+                    pass = false;
+                    break;
+                } else if (reserved.contains(typeReference)) {
+                    System.out.printf("Reserved Word: %20s%n", typeReference);
+                    i += typeReference.length();
+                } else {
+                    System.out.printf("TypeReference: %20s%n", typeReference);
+                    i += typeReference.length();
+                }
+            } else if (Character.isLetter(c) && Character.isLowerCase(c)) {
+                String identifier = getIdentifier(line.substring(i));
+                if (identifier == null) {
+                    System.out.println("Error: Invalid Identifier on line \"" + line + "\"");
+                    pass = false;
+                    break;
+                } else {
+                    System.out.printf("Identifier: %23s%n", identifier);
+                    i += identifier.length();
+                }
+            } else if (Character.isDigit(c)) {
+                String number = getNumber(line.substring(i));
+                if (number == null) {
+                    System.out.println("Error: Invalid number on line \"" + "\"");
+                    pass = false;
+                    break;
+                } else {
+                    System.out.printf("Number: %27s%n", number);
+                    i += number.length();
+                }
+            } else if (rangeSeparator(line, i)) {
+                System.out.printf("Range Separator: %18s%n", "..");
+                i += 2;
+            } else if (charTokens.containsKey(line.charAt(i))) {
+                System.out.printf("Token: %28s%n", charTokens.get(line.charAt(i)));
+                i++;
+            } else {
+                System.out.println("Error: Invalid token on line \"" + line + "\"");
+                pass = false;
+                break;
+            }
+        }
+    }
+
+    public static boolean assignmentOperator(String line, int i) {
+        return line.startsWith("::=", i);
+    }
+
+    public static String getTypeReference(String line) {
+        StringBuilder ans = new StringBuilder();
+        boolean lastWasHyphen = false;
+
+        for (char c : line.toCharArray()) {
+            if (lastWasHyphen) {
+                if (c == '-' || c == ' ' || c == '\0') {
+                    return null;
+                } else {
+                    lastWasHyphen = false;
+                }
+            }
+
+            if (Character.isLetter(c) || Character.isDigit(c) || c == '-') {
+                ans.append(c);
+                if (c == '-') {
+                    lastWasHyphen = true;
+                }
+            } else if (c == ' ' || charTokens.containsKey(c)) {
+                return ans.toString();
+            } else {
+                return null;
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static String getIdentifier(String line) {
+        StringBuilder ans = new StringBuilder();
+        boolean lastWasHyphen = false;
+
+        for (char c : line.toCharArray()) {
+            if (lastWasHyphen) {
+                if (c == '-' || c == ' ' || c == '\0') {
+                    return null;
+                } else {
+                    lastWasHyphen = false;
+                }
+            }
+
+            if (Character.isLetter(c) || Character.isDigit(c) || c == '-') {
+                ans.append(c);
+                if (c == '-') {
+                    lastWasHyphen = true;
+                }
+            } else if (c == ' ' || charTokens.containsKey(c)) {
+                return ans.toString();
+            } else {
+                return null;
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static String getNumber(String line) {
+        StringBuilder ans = new StringBuilder();
+        if (line.charAt(0) == '0') {
+            return null;
+        }
+
+        for (char c : line.toCharArray()) {
+            if (Character.isDigit(c)) {
+                ans.append(c);
+            } else if (c == ' ' || charTokens.containsKey(c)) {
+                return ans.toString();
+            } else {
+                return null;
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static boolean rangeSeparator(String line, int i) {
+        return line.startsWith("..", i);
     }
 }
